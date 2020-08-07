7865119c02424128afe80316db224b90080f9f65
zafarpaswal <paswal@gmail.com>
2020-08-07 11:49:03 +0200

    Update and rename bo_util to bo_util.py

--- bo_util.py
+++ bo_util.py
@@ -12,7 +12,7 @@ def plot_approximation(gpr, X, Y, X_sample, Y_sample, X_next=None, show_legend=F
     plt.plot(X, mu, 'b-', lw=1, label='Surrogate function')
     plt.plot(X_sample, Y_sample, 'kx', mew=3, label='Noisy samples')
     if X_next:
-        plt.axvline(x=X_next, ls='--', c='k', lw=1)
+        plt.axvline(x=X_next, ls='--', c='k', lw=1).
     if show_legend:
         plt.legend()
 
